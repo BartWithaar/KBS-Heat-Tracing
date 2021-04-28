103f29f87133d8a5c1174900516c295c7c04024e
SimonBalk <s1132208@student.windesheim.nl>
2021-04-28 09:44:41 +0200

    temperatuur wordt berekend

--- KBS Heat tracing IoT device/src/main.cpp
+++ KBS Heat tracing IoT device/src/main.cpp
@@ -5,6 +5,10 @@
 #define LED 13
 #define ADCPIN 2
 #define NUMSAMPLES 5
+#define BCOEFFICIENT 3950
+#define TEMPERATURENOMINAL 25
+#define CONVERTO_TO_DEGREES(input) (float((input-240)/1.23))
+
 int samples[NUMSAMPLES];
 int adcData;
 
@@ -22,26 +26,37 @@ void loop() {
   // take N samples in a row, with a slight delay
   for (i=0; i< NUMSAMPLES; i++) {
    samples[i] = analogRead(ADCPIN);
-   delay(10);
   }
  
   // average all the samples out
   average = 0;
   for (i=0; i< NUMSAMPLES; i++) {
      average += samples[i];
+     
   }
   average /= NUMSAMPLES;
 
   Serial.print("Average analog reading "); 
   Serial.println(average);
 
+  //average = (1000*average)/((4095-average)-1);
+  
+
+  Serial.print("Temp: ");
+  Serial.println(CONVERTO_TO_DEGREES(average));
+  Serial.println((average-240)/1.23);
+
+
   //adcData = analogRead(ADCPIN);
   //Serial.println(adcData);
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
+  //digitalWrite(LED, HIGH);
   //Serial.println("LED is on");
   delay(1000);
-  digitalWrite(LED, LOW);
+  //digitalWrite(LED, LOW);
   //Serial.println("LED is off");
   delay(1000);
 }
+
+
+
